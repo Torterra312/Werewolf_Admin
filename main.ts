e8872e6812f7b0738e7994293152ec66532f536c
Torterra312 <97889941+Torterra312@users.noreply.github.com>
2022-01-31 12:34:30 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
-radio.setGroup(173)
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    Player_Amounts += -1
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(randint(1, Player_Amounts))
+    OnOff = 1
+})
+input.onButtonPressed(Button.B, function () {
+    Player_Amounts += 1
+})
+let OnOff = 0
+let Player_Amounts = 0
+radio.setGroup(173)
+Player_Amounts = 0
+OnOff = 0
+if (OnOff == 1) {
+    basic.showString("Game Started")
+} else {
+    basic.showNumber(Player_Amounts)
+}
